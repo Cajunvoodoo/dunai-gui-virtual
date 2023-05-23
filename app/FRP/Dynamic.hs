8ae15df99a460cd4bc52d475a025c5bf526a4be1
Cajunvoodoo <tomdwy1008@gmail.com>
2023-05-23 04:08:47 -0500

    Improve Comments

--- app/FRP/Dynamic.hs
+++ app/FRP/Dynamic.hs
@@ -2,16 +2,20 @@ module FRP.Dynamic where
 
 
 import FRP.BearRiver
--- import FRP.Yampa
 
+-- | Dynamics allow for the subscription to @Events@ while tracking a current
+-- value.
 data Dynamic a = Dynamic
     { modified :: Event a
     , current :: a
     } deriving (Show, Functor)
 
+-- | Retrieve the current value from a @Dynamic@. Functionally identical to
+-- @modified@, but with a name that may make more sense.
 updated :: Dynamic a -> Event a
 updated = modified
 
+-- | Holds an initial value, updating the value whenever an event is received.
 holdDyn :: forall m a. Monad m => a -> SF m (Event a) (Dynamic a)
 holdDyn a = proc ev -> do
   loopPre a loopHold -< ev
@@ -22,6 +26,7 @@ holdDyn a = proc ev -> do
         Event new -> returnA -< (Dynamic newEv new, new)
         NoEvent   -> returnA -< (Dynamic NoEvent old, old)
 
+-- | Like @holdDyn@, but the value in the @Dynamic@ never changes.
 constDyn :: Monad m => a -> SF m () (Dynamic a)
 constDyn a = pure $ Dynamic NoEvent a
 
