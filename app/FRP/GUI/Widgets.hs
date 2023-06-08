cc7ef1cc9765de350937562b66c320b67db274f1
Cajunvoodoo <tomdwy1008@gmail.com>
2023-06-07 19:34:10 -0500

    Widget typeclass outline

--- app/FRP/GUI/Widgets.hs
+++ app/FRP/GUI/Widgets.hs
@@ -1,3 +1,25 @@
 module FRP.GUI.Widgets where
 
 import FRP.BearRiver
+import Data.Kind (Type)
+
+class Widget w where
+
+  -- | The signals the widget can accept
+  -- FIXME: markdown in haddock comments
+  -- Ex. a textbox may accept Append, Delete, Clear, etc
+  data WidgetInput w :: Type
+
+  -- | The signals the widget can output. These should track
+  -- the changes in the widget such that it renders correctly.
+  -- Dynamics are often preferred, as they can hold both the
+  -- change and the whole state.
+  -- FIXME: markdown in haddock comments
+  -- Ex. a textbox may output a @Dynamic@ containing its
+  -- most recent addition and its current whole value
+  data WidgetOutput w :: Type
+
+
+--TODO: find a way to represent how it should be rendered
+-- perhaps a typeclass function that acts in some monad
+-- in order to maintain the state, like what changed and the current tree
