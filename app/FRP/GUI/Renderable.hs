17c51cd2216cd92190b1e51d7bed07ab2ac2d07e
Cajunvoodoo <tomdwy1008@gmail.com>
2023-06-07 19:34:23 -0500

    Renderable outline

---
+++ app/FRP/GUI/Renderable.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
+-- | TODO: summary of Renderable module
+
+module FRP.GUI.Renderable where
+import Data.Kind (Type)
+import Control.Monad.IO.Class
+
+-- | Converts an abstract widget into its backend representation.
+-- This is to be implemented by the backend for each
+class Renderable frontW backW where
+  -- | The output of the rendered widget.
+  -- type family RenderType widget :: Type
+
+  -- | Turn the abstract widget definition (FRP side) into
+  -- its backend-specific representation
+  render
+    :: MonadIO m
+    => frontW
+    -> m backW
