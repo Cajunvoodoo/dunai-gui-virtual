126f00ac34d7b5b99384090aa082f40e184b15a4
Cajunvoodoo <tomdwy1008@gmail.com>
2023-07-27 03:19:17 -0500

    final GTK version

--- dunai-gui-test/app/Main.hs
+++ dunai-gui-test/app/Main.hs
@@ -1,7 +1,100 @@
-module Main where
+-- module Main where
 
-import FRP.GUI.Widgets as W
+-- import FRP.GUI.Widgets as W
 
+
+-- main :: IO ()
+-- main = W.main
+{-# language BlockArguments #-}
+{-# language LambdaCase #-}
+{-# language OverloadedStrings #-}
+
+module Main ( main ) where
+
+import Control.Exception
+import Control.Monad.IO.Class
+import Control.Monad.Managed
+import DearImGui
+import DearImGui.OpenGL3
+import DearImGui.SDL
+import DearImGui.SDL.OpenGL
+import Graphics.GL
+import SDL
+import Control.Concurrent (threadDelay)
 
 main :: IO ()
-main = W.main
+main = do
+  -- Initialize SDL
+  initializeAll
+
+
+  runManaged do
+    -- Create a window using SDL. As we're using OpenGL, we need to enable OpenGL too.
+    window <- do
+      let title = "Hello, Dear ImGui!"
+      let config = defaultWindow { windowGraphicsContext = OpenGLContext defaultOpenGL }
+      managed $ bracket (createWindow title config) destroyWindow
+
+    -- Create an OpenGL context
+    glContext <- managed $ bracket (glCreateContext window) glDeleteContext
+
+
+    -- Create an ImGui context
+    _ <- managed $ bracket createContext destroyContext
+
+    -- Initialize ImGui's SDL2 backend
+    _ <- managed_ $ bracket_ (sdl2InitForOpenGL window glContext) sdl2Shutdown
+
+    -- Initialize ImGui's OpenGL backend
+    _ <- managed_ $ bracket_ (openGL3Init >> swapInterval $= SynchronizedUpdates) openGL3Shutdown
+
+    liftIO $ mainLoop window
+
+
+mainLoop :: Window -> IO ()
+mainLoop window = unlessQuit do
+  -- Tell ImGui we're starting a new frame
+  openGL3NewFrame
+  sdl2NewFrame
+  newFrame
+
+  -- Build the GUI
+  withWindowOpen "Hello, ImGui!" do
+    -- Add a text widget
+    text "Hello, ImGui!"
+
+    -- Add a button widget, and call 'putStrLn' when it's clicked
+    button "Clickety Click" >>= \case
+      False -> return ()
+      True  -> putStrLn "Ow!"
+
+  -- Show the ImGui demo window
+  showDemoWindow
+
+  -- Render
+  glClear GL_COLOR_BUFFER_BIT
+
+  render
+  openGL3RenderDrawData =<< getDrawData
+
+  glSwapWindow window
+
+  threadDelay 10000
+
+  mainLoop window
+
+  where
+    -- Process the event loop
+    unlessQuit action = do
+      shouldQuit <- checkEvents
+      if shouldQuit then pure () else action
+
+    checkEvents = do
+      pollEventWithImGui >>= \case
+        Nothing ->
+          return False
+        Just event ->
+          (isQuit event ||) <$> checkEvents
+
+    isQuit event =
+      SDL.eventPayload event == SDL.QuitEvent
--- dunai-gui-virt/app/FRP/GUI/Widgets.hs
+++ dunai-gui-virt/app/FRP/GUI/Widgets.hs
@@ -5,7 +5,7 @@ import FRP.BearRiver
 import Data.Kind (Type)
 import Control.Monad.Reader (ReaderT, MonadTrans (lift), MonadReader (..), asks)
 
-import GI.Gtk (new, ManagedPtr, Button (Button), AttrOp (..), on, IsBuilder, GObject, builderGetObject, unsafeCastTo, set)
+import GI.Gtk (new, ManagedPtr, AttrOp (..), on, IsBuilder, GObject, builderGetObject, unsafeCastTo, set, get)
 import Data.GI.Base.Attributes
     ( AttrOp, AttrOpTag(AttrConstruct) )
 import Data.GI.Base.BasicTypes ( ManagedPtr )
@@ -16,7 +16,9 @@ import Control.Monad (void)
 import Data.GI.Base.Constructible ( Constructible(..) )
 import qualified GI.Gtk as Gtk
 import qualified GI.Gio as Gio
-import Data.MonadicStreamFunction.ReactHandle (reactInit)
+import qualified GI.Gdk as Gdk
+import qualified GI.GLib as GLib
+import Data.MonadicStreamFunction.ReactHandle (reactInit, react)
 import Control.Monad.IO.Class
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -25,10 +27,10 @@ import Control.Exception (Exception, throw)
 import Data.GI.Base.Signals
 import Data.GI.Base.ShortPrelude
 import GHC.TypeLits (KnownSymbol)
-import GI.Gtk (get)
 import Data.Unique
 import FRP.Dynamic
 import Data.GI.Base.Overloading
+import Control.Monad.Fix (MonadFix)
 
 -- class Widget w where
 --   -- | The signals the widget can accept
@@ -114,6 +116,7 @@ signalEN' self signal f b = performOnFirstSample $ do
 
   pure $ arrM $ \_ -> do
     --Register the event in the event storage and generate a new ID
+    --FIXME: this block may need to be moved up, as it may result in duplicate registrations
     liftIO $ register addHandler $ \a -> do
       unique <- liftIO newUnique --generate a new unique
       writeIORef uniqueStorage unique --put the new unique in its storage
@@ -204,6 +207,7 @@ attrE self attr = proc _ -> do
   retAttr <- arrM $ get self -< attr --get the current attribute's value
   returnA -< tag e retAttr --return the attr given the event fired
 
+-- | Track an attribute
 attrD
   :: ( GObject self
      , AttrGetC info self attr result
@@ -219,14 +223,167 @@ attrD self attr = performOnFirstSample $ do
     ev <- attrE self attr -< ()
     holdDyn initValue -< ev --update the dynamic with the new value
 
+-- attrD'
+--   :: ( GObject self
+--      , AttrGetC info self attr result
+--      , KnownSymbol (AttrLabel info)
+--      , MonadIO m
+--      )
+--   => AttrLabelProxy (attr :: Symbol)
+--   -> SF m self (Dynamic result)
+-- attrD' attr = performOnFirstSample $ do
+--   --this function is a bit convoluted, but it is needed(?) to skirt around the
+--   --restrictions placed by arrows
+
+--   --empty storage, where the future
+--   --value of "self" will be stored
+--   initStorage <- liftIO $ newIORef undefined
+
+--   pure $ proc self -> do
+--     attrVal <- arrM (`get` attr) -< self --get the attribute value
+
+--     --add the initial attribute value into storage
+--     arrM (liftIO . writeIORef initStorage) -< attrVal
+--     --TODO: convert attrE into accepting non-static widget
+--     holdDynNonStatic (readIORef initStorage) -< ev
+--     -- ev <- attrE self attr -< ()
+--     -- holdDyn initValue -< ev --update the dynamic with the new value
+
+-- attrE
+--   :: ( GObject self
+--      , AttrGetC info self attr result
+--      , KnownSymbol (AttrLabel info)
+--      , MonadIO m
+--      )
+--   => self
+--   -> AttrLabelProxy (attr :: Symbol)
+--   -> SF m () (Event result) --m (Event result)
+-- attrE self attr = performOnFirstSample $ do
+--   selfStorage
+--   proc _ -> do
+--   e <- signalE1 self (PropertyNotify attr) -< ()
+--   retAttr <- arrM $ get self -< attr --get the current attribute's value
+--   returnA -< tag e retAttr --return the attr given the event fired
+
+-- TODO: make Button opaque
+newtype Button = Button
+  {
+    gtkButton :: Gtk.Button
+  }
+
+-- mkWidget
+--   :: (MonadIO m)
+--   => (ManagedPtr a -> a)
+--   -> [AttrOp a 'AttrConstruct]
+--   -> SF m [AttrOp a 'AttrSet] a
+-- mkWidget widget initAttrs = performOnFirstSample $ do
+--   w <- new widget initAttrs
+--   pure $ proc attrs -> do
+--     arrM (set w) -< attrs
+--     returnA -< w
+
+mkButton
+  :: MonadIO m
+  => [AttrOp Gtk.Button 'AttrConstruct]
+  -> SF m [AttrOp Gtk.Button 'AttrSet] Button
+mkButton initAttrs = performOnFirstSample $ do
+  button <- new Gtk.Button initAttrs
+  pure $ proc attrs -> do
+    arrM (set button) -< attrs
+    returnA -< Button button
+
+pressed
+  :: forall m
+   . MonadIO m
+  => SF m Button Bool --bool or event?
+pressed = performOnFirstSample $ do
+  evStorage <- liftIO $ newIORef False
+  pure $ proc btn -> do
+    switch (initSF evStorage) (newSF evStorage) -< btn
+  where
+    initSF :: IORef Bool -> SF m Button (Bool, Event Bool)
+    initSF evStorage = proc btn -> do
+      --the point of initSF is to apply @on btn #clicked@ ONCE and ONLY ONCE
+      arrM (\x -> do
+              liftIO $
+                on (gtkButton x) #clicked
+                (writeIORef evStorage True)
+              liftIO $ print "in arrM"
+           ) -< btn
+      returnA -< (False, Event False) --we always want to immediately switch
+    newSF :: IORef Bool -> Bool -> SF m Button Bool
+    newSF evStorage = const $ proc btn -> do --the event we receive does not matter
+      -- status is written to when the button is pressed
+      status <- arrM (const $ liftIO $ readIORef evStorage) -< ()
+      -- reset the status
+      arrM (liftIO . writeIORef evStorage) -< False
+      returnA -< status
+
+
+
+
+testWidget :: (MonadIO m, MonadFix m) => Gtk.Application -> SF m () ()
+testWidget app = performOnFirstSample $ do
+  w <- new Gtk.Window [ #application := app
+                      , #title := "Haskell Gi - Examples - Hello World"
+                      , #defaultHeight := 200
+                      , #defaultWidth := 200
+                      ]
+  Gtk.applicationAddWindow app w
+
+  bbox <- new Gtk.Box [ #orientation := Gtk.OrientationHorizontal ]
+  Gtk.setWindowChild w bbox
+
+  pure $ proc _ -> do
+    myButton <-
+        mkButton [
+                  #label := "This is the yampa one"
+                ] -< []
+    otherBtn <-
+        mkButton [
+                  #label := "other btn"
+                ] -< []
+    rec
+      arrM (uncurry set) -< (gtkButton myButton, [ #useUnderline := otherBtnPressed ])
+      arrM (uncurry set) -< (gtkButton myButton, [ #useUnderline := pressedEv ])
+      pressedEv <- pressed -< myButton
+      otherBtnPressed <- pressed -< otherBtn
+    arrM $ liftIO . putStr -< if pressedEv then "output from the button!!!!!!\n" else ""
+
+    performSFOnce $ arrM (\(myButton, otherBtn) -> do
+              #append bbox (gtkButton myButton)
+              #append bbox (gtkButton otherBtn)
+
+              Gtk.widgetSetVisible w True
+              return ()
+            ) -< (myButton, otherBtn)
+
+-- | Perform a SF once, returning that result afterwards
+performSFOnce :: forall m a b. Monad m => SF m a b -> SF m a b
+performSFOnce sf = proc a -> do
+  switch initSF holdSF -< a
+  where
+    initSF :: SF m a (b, Event b)
+    initSF = proc a -> do
+      out <- sf -< a
+      returnA -< (out, Event out)
+    holdSF :: b -> SF m a b
+    holdSF out = proc _ -> do
+      returnA -< out
+
+
+-- >>> :t switch
+-- switch :: Monad m => SF m a (b, Event c) -> (c -> SF m a b) -> SF m a b
+
+
 data AttrOpSF self tag m inp where
   (:->)
     :: ( HasAttributeList self
-       , info ~ ResolveAttribute attr self 
+       , info ~ ResolveAttribute attr self
        , AttrInfo info
        , AttrBaseTypeConstraint info self
        , AttrOpAllowed tag info self
-       , AttrSetTypeConstraint info b 
+       , AttrSetTypeConstraint info b
        , MonadIO m
        )
     => AttrLabelProxy (attr :: Symbol)
@@ -239,18 +396,75 @@ infixr 0 :->
 -- @
 -- myLabel
 -- sink myLabel
-sink 
+sink
   :: ( GObject self
      , MonadIO m
      )
-  => self 
-  -> AttrOpSF self AttrSet m a 
+  => self
+  -> AttrOpSF self AttrSet m a
   -> SF m a ()
 sink self (attr :-> sf) = proc a -> do
   sfOut <- sf -< a
-  -- TODO: see if setting without diffing in sink1 causes 
+  -- TODO: see if setting without diffing in sink1 causes
   -- unnecessary recomputation
   arrM (\x -> set self [attr := x]) -< sfOut
+
+-- | Create a widget once using statically known constants
+mkWidgetKnown
+   :: ( Constructible a tag
+      , MonadIO m
+      )
+   => (ManagedPtr a -> a) -- ^ The widget in question. Example: @GI.Gtk.Button@
+   -> [AttrOp a tag]
+   -> SF m () a
+mkWidgetKnown widget attrs = performOnFirstSample $ do
+  w <- new widget attrs
+  pure $ proc _ -> do
+    returnA -< w
+
+-- | Create a widget using dynamically created attributes. The widget will not
+-- react to these attributes. A new widget will be created for every event
+-- this SF receives!
+mkWidgetStatic
+  :: ( Constructible a tag
+     , MonadIO m
+     )
+  => (ManagedPtr a -> a) -- ^ The widget in question. Example: @GI.Gtk.Button@
+  -> SF m (Event [AttrOp a tag]) (Event a)
+mkWidgetStatic widget = proc attrE -> do
+  case attrE of
+    -- return a new Event containing the new widget
+    Event attrs -> Event <$> arrM (new widget) -< attrs
+    -- return nothing
+    NoEvent     -> returnA -< NoEvent
+
+-- -- | Create a widget using dynamically created attributes. The widget will not
+-- -- react to these attributes. Only one widget will be created.
+-- -- FIXME: determine if it does react, and if it flashes due to constant changes
+-- mkWidgetOnce
+--   :: ( Constructible a tag
+--      , MonadIO m
+--      )
+--   => (ManagedPtr a -> a) -- ^ The widget in question. Example: @GI.Gtk.Button@
+--   -> SF m [AttrOp a tag] a
+-- mkWidgetOnce widget = performOnFirstSample $ do
+--   --FIXME: does this widget flash because of the use of empty attributes?
+--   w <- new widget []
+--   pure $ proc attrs -> do
+--     arrM (set self) -< attrs -- apply the attributes
+
+
+-- | Create a widget once
+mkWidgetReact
+  :: ( Constructible a tag
+     , MonadIO m
+     )
+  => (ManagedPtr a -> a) -- ^ The widget in question. Example: @GI.Gtk.Button@
+  -> SF m (Event [AttrOp a tag]) (Event a)
+mkWidgetReact = undefined
+
+
+
 
 ------------------------------
 -- BASIC HELLO WORLD IN GTK --
@@ -271,11 +485,10 @@ activateApp app = do
 
   bbox <- new Gtk.Box [ #orientation := Gtk.OrientationHorizontal ]
   Gtk.setWindowChild w bbox
-  -- #add w bbox
 
   btn <- new Gtk.Button [ #label := "Hello World!"]
   on btn #clicked printHello
-  on btn #clicked $ Gtk.windowDestroy w
+
   #append bbox btn
 
   Gtk.widgetSetVisible w True
@@ -287,11 +500,13 @@ activateApp app = do
 
 main :: IO ()
 main = do
-  -- reactHandle <- reactInit $ runReaderS_ foo undefined
   app <- new Gtk.Application [ #applicationId := "haskell-gi.examples.hello-world"
                              , #flags := [ Gio.ApplicationFlagsFlagsNone ]
                              ]
+  reactHandle <- reactInit $ runReaderS_ (testWidget app) 0.1
   on app #activate $ activateApp app
+
+  GLib.idleAdd 1 (react reactHandle >> pure True)
   Gio.applicationRun app Nothing
   -- void . forkIO $ do
   --   react reactHandle
